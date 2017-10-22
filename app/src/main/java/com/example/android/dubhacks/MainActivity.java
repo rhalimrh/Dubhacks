8bfc9b2092eb69c71641ae89d8b5bf70c34feae6
Ryan Halim <rhalim14@uw.ed>
2017-10-21 22:12:34 -0700

    Skeleton

---
+++ app/src/main/java/com/example/android/dubhacks/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.dubhacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Finds users when button is pressed
+    public void pressButton(View view) {
+
+    }
+
+
+}
